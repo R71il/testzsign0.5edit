9d7afadf52d5f5813a92b9c56e6fd7a92bfa2e13
R71il <141807300+R71il@users.noreply.github.com>
2025-06-15 16:11:26 +0300

    Update bundle.h

--- HeaderFiles/bundle.h
+++ HeaderFiles/bundle.h
@@ -6,41 +6,42 @@
 class ZAppBundle
 {
 public:
-	ZAppBundle();
+    ZAppBundle();
 
 public:
-	bool SignFolder(ZSignAsset *pSignAsset,
-					const string &strFolder,
-					const string &strBundleID,
-					const string &strBundleVersion,
-					const string &strDisplayName,
-					const string &strDyLibFile,
-					bool bForce,
-					bool bWeakInject,
-					bool bEnableCache,
-					bool excludeProvisioning); // <-- تمت إضافة هذا الخيار
+    bool SignFolder(ZSignAsset *pSignAsset,
+                    const string &strFolder,
+                    const string &strBundleID,
+                    const string &strBundleVersion,
+                    const string &strDisplayName,
+                    const string &strDyLibFile,
+                    bool bForce,
+                    bool bWeakInject,
+                    bool bEnableCache,
+                    bool excludeProvisioning,
+                    bool bInjectToFrameworks); // تمت إضافة البارامتر الأخير
 
 private:
-	bool SignNode(JValue &jvNode);
-	void GetNodeChangedFiles(JValue &jvNode);
-	void GetChangedFiles(JValue &jvNode, vector<string> &arrChangedFiles);
-	void GetPlugIns(const string &strFolder, vector<string> &arrPlugIns);
+    bool SignNode(JValue &jvNode);
+    void GetNodeChangedFiles(JValue &jvNode);
+    void GetChangedFiles(JValue &jvNode, vector<string> &arrChangedFiles);
+    void GetPlugIns(const string &strFolder, vector<string> &arrPlugIns);
 
 private:
-	bool FindAppFolder(const string &strFolder, string &strAppFolder);
-	bool GetObjectsToSign(const string &strFolder, JValue &jvInfo);
-	bool GetSignFolderInfo(const string &strFolder, JValue &jvNode, bool bGetName = false);
+    bool FindAppFolder(const string &strFolder, string &strAppFolder);
+    bool GetObjectsToSign(const string &strFolder, JValue &jvInfo);
+    bool GetSignFolderInfo(const string &strFolder, JValue &jvNode, bool bGetName = false);
 
 private:
-	bool GenerateCodeResources(const string &strFolder, JValue &jvCodeRes);
-	void GetFolderFiles(const string &strFolder, const string &strBaseFolder, set<string> &setFiles);
+    bool GenerateCodeResources(const string &strFolder, JValue &jvCodeRes);
+    void GetFolderFiles(const string &strFolder, const string &strBaseFolder, set<string> &setFiles);
 
 private:
-	bool m_bForceSign;
-	bool m_bWeakInject;
-	string m_strDyLibPath;
-	ZSignAsset *m_pSignAsset;
+    bool m_bForceSign;
+    bool m_bWeakInject;
+    string m_strDyLibPath;
+    ZSignAsset *m_pSignAsset;
 
 public:
-	string m_strAppFolder;
+    string m_strAppFolder;
 };
